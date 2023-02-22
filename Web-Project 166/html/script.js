a3cb9c922ab0a7609534ecf96bf555b1f403ecfe
Thesoysaucekid <bnguyen9991@outlook.com>
2023-02-22 15:04:49 -0700

    update

---
+++ Web-Project 166/html/script.js
@@ -0,0 +1,27 @@
+var tableData = [
+    {
+    'first_name':'Brian' , 
+    'last_name': 'Nguyen' , 
+    'date':'8/15/99',
+    },
+    {
+        'first_name':'Brandon' , 
+        'last_name': 'Nguyen' , 
+        'date':'8/30/20',
+        },
+        {
+            'first_name':'Ethan' , 
+            'last_name': 'Nguyen' , 
+            'date':'8/21/13',
+            },
+            {
+                'first_name':'Ben' , 
+                'last_name': 'Nguyen' , 
+                'date':'8/27/10',
+                },
+                    
+]
+
+for (var i in tableData){
+    var row = 
+}
